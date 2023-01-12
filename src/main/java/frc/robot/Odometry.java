a89349b37b1de601b8902798cbc7babdcb6964eb
ThraceAerospace <boyspockie@gmail.com>
2023-01-11 19:08:10 -0500

    [QUF] messed up a merge conflict, remove unused functions wrote getAverageEncoder method

--- src/main/java/frc/robot/Odometry.java
+++ src/main/java/frc/robot/Odometry.java
@@ -10,6 +10,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import frc.lib.sensors.Pigeon;
 import frc.robot.subsystems.DriveTrain;
+import static frc.robot.Constants.Odometry.*;
 
 class Odometry{
     private DriveTrain driveTrain;
