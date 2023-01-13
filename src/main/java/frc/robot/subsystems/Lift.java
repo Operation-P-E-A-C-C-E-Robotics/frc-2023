f87d59be512cd560d1c2094eff6e8de6402c27c9
phlogiston1 <sdbazoes@gmail.com>
2023-01-13 18:41:06 -0500

    [FET] start lift subsystem

---
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Lift extends SubsystemBase {
+    /** Creates a new ExampleSubsystem. */
+    public Lift() {
+    }
+
+    /**
+     * set the lift motors as a percentage (-1 to 1)
+     * of full power
+     * @param speed the percentage, positive drives out
+     */
+    public void setPercent(double speed){
+        //todo
+    }
+
+    /**
+     * get the distance from the pivot to the end of the
+     * lift
+     * @return lift extension meters
+     */
+    public double getExtension(){
+        return 0; //todo
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
