e029949a6c30b02e67a911a9bdb65a5c4c97e9d3
phlogiston1 <sdbazoes@gmail.com>
2023-01-13 18:44:14 -0500

    [FET] start pivot subsystem

---
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Pivot extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  public Pivot() {}
+
+  public void setPercent(double speed){
+    //todo
+  }
+
+  public void setAnglePID(Rotation2d angle){
+    //todo
+  }
+
+  public void setAngle(Rotation2d angle){
+    //todo
+  }
+
+  public Rotation2d getAngle(){
+    return null; //todo
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
