93a8b5689884fe63777b0347abcd815844d7b3ae
phlogiston1 <sdbazoes@gmail.com>
2023-01-14 12:56:45 -0500

    [QUF] fix constants git conflict

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,47 +25,16 @@ public final class Constants {
 
     public final class OperatorInterface {
         public static final int DRIVER_JOYSTICK = 0;
-    }
-
-    public final class Auto {
-        public static final int PIGEON_IMU  = 0,
-                                TRACK_WIDTH = 0; //TODO Configure
-
-        public static final double kS = 0,
-                                  kV = 0,
-                                  kA = 0,
-                                  kP = 0,
-                                  kI = 0,
-                                  kD = 0,
-                                  RAMSETE_B = 0,
-                                  RAMSETE_ZETA = 0,
-                                  AUTO_VOLTAGE_MAX = 10,
-                                  AUTO_MAX_SPEED_METERS_PER_SECOND = 0,
-                                  AUTO_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 0;
-    }
+    }s
 
     public final class Kinematics {
         public static final double PIVOT_HEIGHT = 0; //TODO how high the pivot is above the ground
 
     }
 
-
-    public final class DriveTrain {
-        public static final int LEFT_MASTER  = 0,
-                                LEFT_SLAVE   = 1,
-                                RIGHT_MASTER = 2,
-                                RIGHT_SLAVE  = 3;
-
-
-    }
-
     public final class Wrist{
         public static final double LENGTH = 0.1,
                                   LENGTH_TO_PLACE = 0;
-    }
-
-    public final class OperatorInterface {
-        public static final int DRIVER_JOYSTICK = 0;
     }
 
     public final class Auto {
