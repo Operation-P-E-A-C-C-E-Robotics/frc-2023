275f9c26c72486cd4dc07d44cada0e8aee1cf550
phlogiston1 <sdbazoes@gmail.com>
2023-01-15 09:00:15 -0500

    [FET] clean up PathFollower (experiment)

--- src/main/java/frc/robot/commands/auto/paths/PathFollower.java
+++ src/main/java/frc/robot/commands/auto/paths/PathFollower.java
@@ -1,17 +1,11 @@
 package frc.robot.commands.auto.paths;
 
 import edu.wpi.first.math.controller.LTVUnicycleController;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.Trajectory.State;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive.WheelSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import frc.robot.Odometry;
 import frc.robot.subsystems.DriveTrain;
 
@@ -24,48 +18,66 @@ public class PathFollower extends CommandBase{
     private Timer timer;
     private DifferentialDriveWheelSpeeds prevSpeeds;
 
-    private SimpleMotorFeedforward feedforward;
-    private PIDController leftController, rightController;
-
     private double prevTime;
     private Odometry odometry;
 
+    /**
+     * experimental path follower, to
+     *  a. allow us to use an {@link LTVUnicycleController}, which
+     *      I hear is better, and
+     *  b. get more control over parameters and velocity control
+     *      than with {@link edu.wpi.first.wpilibj2.command.RamseteCommand}
+     * @param driveTrain drivetrain subsystem
+     * @param trajectory trajectory to follow
+     * @param odometry robot odometry.
+     */
     public PathFollower(DriveTrain driveTrain, Trajectory trajectory, Odometry odometry){
         this.driveTrain = driveTrain;
         this.trajectory = trajectory;
         this.odometry = odometry;
         timer = new Timer();
+        addRequirements(driveTrain);
     }
 
     @Override
     public void initialize() {
         var initialState = trajectory.sample(0);
-        timer.reset();
+
+        //get initial wheel speeds (to find initial acceleration)
         prevSpeeds = odometry.getKinematics().toWheelSpeeds(new ChassisSpeeds(
-            initialState.velocityMetersPerSecond,
-            0,
+        initialState.velocityMetersPerSecond,
+        0,
             initialState.curvatureRadPerMeter * initialState.velocityMetersPerSecond
         ));
+
+        //reset pid controllers
         driveTrain.resetVelocityDrive();
+
+        //reset timer
         prevTime = -1;
+        timer.reset();
         timer.start();
     }
 
     @Override
     public void execute() {
         var time = timer.get();
-        var dt = time - prevTime;
+        var dt = time - prevTime; //time discretization step, whatevertf that means
 
+        //account for time between init and execute.
         if (prevTime < 0){
             driveTrain.tankDriveVolts(0, 0);
             prevTime = time;
             return;
         }
 
+        //get chassis speeds from controller
         var chassis = controller.calculate(odometry.getPose(), trajectory.sample(time));
 
+        //get wheel speeds
         var speeds = odometry.getKinematics().toWheelSpeeds(chassis);
 
+        //drive
         driveTrain.velocityDrive(speeds, prevSpeeds, dt);
 
         prevSpeeds = speeds;
@@ -75,7 +87,7 @@ public class PathFollower extends CommandBase{
 
     @Override
     public void end(boolean interrupted) {
-
+        driveTrain.tankDriveVolts(0, 0);
     }
     @Override
     public boolean isFinished() {
