03d67ebf6a7fe4f48d190a4038768cfdfffdc6aa
phlogiston1 <sdbazoes@gmail.com>
2023-01-21 21:25:28 -0500

    [FET] apriltag limelight code

--- src/main/java/frc/lib/sensors/ApriltagLimelight.java
+++ src/main/java/frc/lib/sensors/ApriltagLimelight.java
@@ -1,13 +1,36 @@
 package frc.lib.sensors;
 
-import edu.wpi.first.networktables.DoubleArrayTopic;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.math.controller.DifferentialDriveAccelerationLimiter;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.*;
 
 public class ApriltagLimelight {
-    NetworkTableInstance networkTables = NetworkTableInstance.getDefault();
-    NetworkTable limelight = networkTables.getTable("limelight");
-    DoubleArrayTopic botposeTopic = limelight.getDoubleArrayTopic("botpose");
+    private final static String NETWORKTABLES_NAME = "limelight"; //TODO
+    private final DoubleArraySubscriber botposeSub;
 
-    
+    public ApriltagLimelight(){
+        NetworkTableInstance networkTables = NetworkTableInstance.getDefault();
+        NetworkTable limelight = networkTables.getTable(NETWORKTABLES_NAME);
+        DoubleArrayTopic botposeTopic = limelight.getDoubleArrayTopic("botpose");
+        botposeSub = botposeTopic.subscribe(new double[0]);
+    }
+
+    public void updatePoseEstimator(DifferentialDrivePoseEstimator estimator){
+        TimestampedDoubleArray[] visionMeasurements = botposeSub.readQueue();
+        for(TimestampedDoubleArray i : visionMeasurements){
+            System.out.println(i);
+            double time = i.serverTime;
+            double[] val = i.value;
+            System.out.println(val.length);
+            if(val.length == 6) {
+                estimator.addVisionMeasurement(new Pose2d(
+                        val[0], //TODO these indices are probably wrong
+                        val[1],
+                        new Rotation2d(val[3])
+                ),time); //TODO varying std deviations based on distance from tags
+            }
+        }
+    }
 }
