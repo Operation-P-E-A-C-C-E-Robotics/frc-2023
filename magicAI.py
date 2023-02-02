cfa7fc555aa02fcc33cb0c8e5566d3a00081f477
= <=>
2023-02-01 20:28:10 -0500

    more ai

--- magicAI.py
+++ magicAI.py
@@ -1,3 +1,7 @@
+#IMPORTANTE NOTE: This is a test of the emergency broadcast system. This is only a test.
+#THIS WAS WRITTEN BY GITHUB USER @FawkinWerld
+#WHAT I WAS TYPING WAS THAT THIS IS ACTUALLY WRITTEN BY GITHUB COPILOT!!!!!!
+#GOOD JOB COPILOT!!!!
 #HELO WORLD!!!!
 #This is a test of the emergency broadcast system. This is only a test.
 #This is a test of the emergency broadcast system. This is only a test.
@@ -19,6 +23,12 @@ def main():
     print("Now I'm done")
     print("New line")
     print("never gonna give you up")
+    print("never gonna let you down")git
+    print("never gonna run around and desert you")
+    print("never gonna make you cry")
+    print("never gonna say goodbye")
+    print("never gonna tell a lie and hurt you")
+    print("never gonna give you up")
     print("never gonna let you down")
     print("never gonna run around and desert you")
     print("never gonna make you cry")
@@ -29,6 +39,10 @@ def main():
     print("never gonna run around and desert you")
     print("never gonna make you cry")
     print("never gonna say goodbye")
+    print("never gonna tell a lie and hurt you")
+    print("never gonna give you up")
+    print("never gonna let you down")
+
 
 if __name__ == "__main__":
     main()
